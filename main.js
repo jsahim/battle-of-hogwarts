80d10728770d83942612dd888d913c84e5cbc471
Jeff Sahim <jeffreysahim@gmail.com>
2023-01-14 14:26:30 -0500

    Add more query selectors

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // GLOBAL VARIABLES
-var humanObject 
-var computerObject 
+var humanInstance 
+var computerInstance
 var currentGame
 
 // QUERY SELECTORS
@@ -8,9 +8,19 @@ var spellIcons = document.querySelectorAll(".spell-icons")
 var classicBox = document.getElementById("classicGameBox")
 var difficultBox = document.getElementById("difficultGameBox")
 var changeGameButton = document.getElementById("gameButton")
+var gameSelectionScreen = document.getElementById("chooseGameScreen")
+var classicFighterScreen = document.getElementById("classicFighterScreen")
+var difficultFighterScreen = document.getElementById("difficultFighterScreen")
+var winResultsScreen = document.getElementById("winResultsScreen")
 
 //EVENT LISTENERS
+window.addEventListener("load", setGameComponents)
 
+function setGameComponents(){
+  humanInstance = new Player("Harry")
+  computerInstance = new Player("Voldemort")
+  currentGame = new Game (humanInstance, computerInstance)
+}
 
 // function showGameSelectionScreen(){
 // }
