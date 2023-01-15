f0aa287dc2d47533640945841b56bd359b55ad5a
Jeff Sahim <jeffreysahim@gmail.com>
2023-01-15 12:05:36 -0500

    Complete button functionality

--- main.js
+++ main.js
@@ -18,12 +18,52 @@ var computerScore = document.getElementById("computerScore")
 
 //EVENT LISTENERS
 window.addEventListener("load", setGameComponents)
-classicBox.addEventListener("click", showChooseFighterScreen)
-difficultBox.addEventListener("click", showChooseFighterScreen)
+classicBox.addEventListener("click", assignGameType)
+difficultBox.addEventListener("click", assignGameType)
 changeGameButton.addEventListener("click", showGameSelectionScreen)
 spellIcons.forEach(
   event => event.addEventListener("click", selectSpell)
 )
+
+//FUNCTIONS FOR SCREEN DISPLAY
+
+function showGameSelectionScreen(){
+  subHeading.innerText = "Choose Your Game!"
+  gameSelectionScreen.classList.remove('hidden')
+  classicFighterScreen.classList.add('hidden')
+  difficultFighterScreen.classList.add('hidden')
+  winResultsScreen.classList.add('hidden')
+  changeGameButton.classList.add('hidden')
+}
+
+function showChooseFighterScreen(){
+  subHeading.innerText = "Choose Your Spell!"
+  if(currentGame.type === "classic"){
+    gameSelectionScreen.classList.add('hidden')
+    classicFighterScreen.classList.remove('hidden')
+    difficultFighterScreen.classList.add('hidden')
+    winResultsScreen.classList.add('hidden')
+    assessButtonUse()
+  } else {
+    gameSelectionScreen.classList.add('hidden')
+    classicFighterScreen.classList.add('hidden')
+    difficultFighterScreen.classList.remove('hidden')
+    winResultsScreen.classList.add('hidden')
+    assessButtonUse()
+  }
+}
+
+function showWinnerScreen(){
+  gameSelectionScreen.classList.add('hidden')
+  classicFighterScreen.classList.add('hidden')
+  difficultFighterScreen.classList.add('hidden')
+  winResultsScreen.classList.remove('hidden')
+  changeGameButton.classList.add('hidden')
+}
+
+
+
+//FUNCTIONS FOR GAME PLAY
 
 function setGameComponents(){
   humanInstance = new Player("Harry", "⚡️")
@@ -31,11 +71,23 @@ function setGameComponents(){
   currentGame = new Game (humanInstance, computerInstance)
 }
 
+function assignGameType(event){
+  currentGame.type = event.target.dataset.parent
+  showChooseFighterScreen()
+}
+
 function selectSpell(event){
   var chosenSpell = event.target.id
   humanInstance.takeTurn(event, chosenSpell)
 }
 
+function assessButtonUse(){
+  if(currentGame.roundsCompleted === 0){
+    changeGameButton.classList.add('hidden')
+  } else {
+    changeGameButton.classList.remove('hidden')
+  }
+}
 
 function displayResults(winnerInstance, userSpell, compSpell){
   winResultsScreen.innerHTML = " "
@@ -50,42 +102,6 @@ function displayResults(winnerInstance, userSpell, compSpell){
     subHeading.innerText = `${winnerInstance.token} ${winnerInstance.name} ${winnerInstance.token} won this battle!`
   }
   showWinnerScreen()
-  setTimeout(assessGameType, 2000)
+  setTimeout(showChooseFighterScreen, 2000)
 }
 
-function showGameSelectionScreen(){
-  gameSelectionScreen.classList.remove('hidden')
-  classicFighterScreen.classList.add('hidden')
-  difficultFighterScreen.classList.add('hidden')
-  winResultsScreen.classList.add('hidden')
-  changeGameButton.classList.add('hidden')
-}
-
-function assessGameType(){
-  changeGameButton.classList.remove('hidden')
-  showChooseFighterScreen()
-}
-
-function showChooseFighterScreen(event){
-  subHeading.innerText = "Choose Your Spell!"
-  if(currentGame.type === "classic" || (event.target.parentNode.id === "classicGameBox" && currentGame.roundsCompleted === 0)){
-    gameSelectionScreen.classList.add('hidden')
-    classicFighterScreen.classList.remove('hidden')
-    difficultFighterScreen.classList.add('hidden')
-    winResultsScreen.classList.add('hidden')
-  } else {
-    currentGame.type = "difficult"
-    gameSelectionScreen.classList.add('hidden')
-    classicFighterScreen.classList.add('hidden')
-    difficultFighterScreen.classList.remove('hidden')
-    winResultsScreen.classList.add('hidden')
-  }
-}
-
-function showWinnerScreen(){
-  gameSelectionScreen.classList.add('hidden')
-  classicFighterScreen.classList.add('hidden')
-  difficultFighterScreen.classList.add('hidden')
-  winResultsScreen.classList.remove('hidden')
-  changeGameButton.classList.add('hidden')
-}
